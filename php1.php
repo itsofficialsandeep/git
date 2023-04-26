14f9c6efc9a6892de0ee30509a80bce66e02433a
itsofficialsandeep <sksandeepsharma25@gmail.com>
2023-04-26 10:23:55 +0530

    i was just learning

---
+++ php1.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<h2>this line has been added after git bash</h2>
+</body>
+</html>
+
---
+++ php2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+
+<p>My first paragraph 1.</p>
+<p>My first paragraph 2.</p>
+<p>My first paragraph 3.</p>
+
+<p>sandeep kumar</p>
+
+
+</body>
+</html>
+
